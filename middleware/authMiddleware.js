d5474ec4e81da1717bae0069929ba7b07e7f49e2
gatteoelmo <matteogallardo99@gmail.com>
2025-01-16 12:18:17 +0000

    middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,13 +1,20 @@
 import jwt from 'jsonwebtoken';
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ message: 'Accesso negato, token mancante' });
+  // Recupera il token dall'header Authorization
+  const authHeader = req.header('Authorization');
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Accesso negato, token mancante' });
+  }
+
+  // Rimuovi il prefisso "Bearer " dal token
+  const token = authHeader.replace('Bearer ', '');
 
   try {
+    // Verifica il token
     const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified;
-    next();
+    req.user = verified; // Salva i dati del token in req.user
+    next(); // Passa al prossimo middleware o al controller
   } catch (error) {
     res.status(400).json({ message: 'Token non valido' });
   }
